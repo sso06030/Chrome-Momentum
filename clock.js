90fe5a51231447ebdc52c9db6cffaf3965fc14e8
kim-yeji <sso06030@khu.ac.kr>
2020-07-19 23:08:22 +0900

    weather and temp

--- clock.js
+++ clock.js
@@ -5,8 +5,7 @@ function getTime(){
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours<10 ? `0${hours}` : hours}:${minutes<10 ? `0${minutes}` : minutes}:${seconds <10 ? `0${seconds}` : seconds}`;
+    clockTitle.innerText = `${hours<10 ? `0${hours}` : hours}:${minutes<10 ? `0${minutes}` : minutes}`;
 
 }
 
